fc66cd6ff3d6b47d3522e0cf8077499ac73d5ab7
Martin Paulo <martin.paulo@gmail.com>
2015-09-14 11:15:14 +1000

    Described changes

--- chapter1.py
+++ chapter1.py
@@ -1,3 +1,9 @@
+# More or less a straight riff of the original documentation for chapter 1.
+# There are two differences, though:
+#       We put the configuration into a config file, so that we can share it amongst files, and not accidentally
+#           check it into source control.
+#       We have modified the network routines to deal with NeCTAR's use of private IP's
+
 # step-1
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
